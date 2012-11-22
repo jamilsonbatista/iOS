7e9404c0236cf5da054e1fe38e18e6d721be3021
Phil Laird <lucaboq@gmail.com>
2012-11-21 16:16:59 -0800

    Added the iOS SDK and example app

---
+++ TapForTap/TapForTapInterstitial.h
@@ -0,0 +1,28 @@
+//
+//  TapForTapInterstitial.h
+//  TapForTapAds
+//
+//  Created by Sami Samhuri on 12-09-05.
+//  Copyright (c) 2012 Tap for Tap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol TapForTapInterstitialDelegate <NSObject>
+- (void) tapForTapInterstitialWasDismissed;
+@end
+
+////
+
+@interface TapForTapInterstitial : NSObject
+
++ (void) prepare;
++ (BOOL) isReady;
++ (void) setDelegate: (id<TapForTapInterstitialDelegate>) delegate;
++ (void) showWithRootViewController: (UIViewController *)rootViewController;
+
+- (BOOL) isReady;
+- (void) showWithRootViewController: (UIViewController *)rootViewController;
+
+@end
